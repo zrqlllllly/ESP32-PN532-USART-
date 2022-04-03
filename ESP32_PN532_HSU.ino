af9746f9ce51eb263303a1f420419743c4dfee57
zrqlllllly <66728639+zrqlllllly@users.noreply.github.com>
2022-04-03 22:54:10 +0800

    Add files via upload

---
+++ ESP32_PN532_HSU.ino
@@ -0,0 +1,63 @@
+#include <PN532_HSU.h>
+#include <PN532.h>
+#define RXD2 3
+#define TXD2 1
+
+PN532_HSU pn532hsu(Serial);
+PN532 nfc(pn532hsu);
+int a;
+void setup(void) {
+  pinMode(23,INPUT); 
+  Serial.begin(115200, SERIAL_8N1, RXD2, TXD2);
+  //Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
+
+  Serial.println("Hello!");
+
+  nfc.begin();
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    Serial.print("Didn't find PN53x board");
+    while (1); // halt
+  }
+  // Got ok data, print it out!
+  Serial.print("Found chip PN5"); Serial.println((versiondata >> 24) & 0xFF, HEX);
+  Serial.print("Firmware ver. "); Serial.print((versiondata >> 16) & 0xFF, DEC);
+  Serial.print('.'); Serial.println((versiondata >> 8) & 0xFF, DEC);
+
+  // configure board to read RFID tags
+  nfc.SAMConfig();
+
+  Serial.println("Waiting for an ISO14443A Card ...");
+}
+
+
+void loop(void) {
+a=digitalRead(23);
+  if(a==LOW)         // 当检测到按键按下时
+{
+ uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+
+  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
+  // 'uid' will be populated with the UID, and uidLength will indicate
+  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+
+  if (success) {
+    // Display some basic information about the card
+    Serial.println("Found an ISO14443A card");
+    Serial.print("  UID Length: "); Serial.print(uidLength, DEC); Serial.println(" bytes");
+    Serial.print("  UID Value: ");
+    nfc.PrintHex(uid, uidLength);
+    Serial.println("");
+
+
+
+    
+  }
+}
+
+ 
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# ESP32-PN532-USART-
-ESP32+PN532(USART)
+# GET-SERIAL-ESP32-PN532-SPI
+Contoh membaca serial kartu menggunakan eps32 mengunakan reader pn532
+
+## Peralatan yang dibutuhkan adalah
+1. Eps32
+2. Reader pn532
+3. Smart card 
+4. [Library PN532](https://github.com/elechouse/PN532)
+
+## Schematic
+<figure style="text-align: center">
+                  <img src="schematic.png" alt="Schematic"/>
+                  <figcaption style="text-align: center; font-weight: bold">Schematic</figcaption>
+              </figure>
